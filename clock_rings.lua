838e57d6316de701360bebe9513d3eccca1dd64b
zyw_0618 <zyw_0618@outlook.com>
2024-06-15 15:34:21 +0800

    Fix attempt to call a nil value global cairo_xlib_surface_create

--- clock_rings.lua
+++ clock_rings.lua
@@ -233,6 +233,7 @@ clock_alpha=0.5
 show_seconds=true
 
 require 'cairo'
+require 'cairo_xlib'
 
 function rgb_to_r_g_b(colour,alpha)
 	return ((colour / 0x10000) % 0x100) / 255., ((colour / 0x100) % 0x100) / 255., (colour % 0x100) / 255., alpha
